c23ce8dc669b09b2ae71c9c1b0ef861a23b2d5ee
Natyes-ops <gurit.nat@bk.ru>
2022-05-02 21:53:05 +0300

    fix: change function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -17,39 +17,45 @@ function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   } 
-  let result = arr.slice(0);
+  let result = arr.slice(0),
+      i = 0;
   if (result.length == 0) {
     return arr;
   } 
-  result.map((item, i) => {
-    if (item === '--discard-next') {
+  while (i < result.length) {
+    console.log(result);
+    if (result[i] === '--discard-next') {
       if (i == result.length - 1) {
-        result.splice(i,1)
+        result.pop();
+        i = 0;
       } else {
-        result.splice(i,2)
+        result.splice(i,2);
       }
-    } else if (item === '--discard-prev') {
+    } else if (result[i] === '--discard-prev') {
       if (i == 0) {
-        result.splice(i,1)
+        result.unshift(result[i]);
       } else {
-        result.splice(--i,2)
+        result.splice(--i,2);
       }
-    } else if (item === '--double-next') {
+    } else if (result[i] === '--double-next') {
       if (i == result.length - 1) {
-        result.splice(i, 1)
+        result.push(result[i]);
       } else {
-        result.splice(i, 1,result[++i])
+        result.splice(i, 1, result[++i]);
       }
-    } else if (item === '--double-prev') {
+    } else if (result[i] === '--double-prev') {
       if (i == 0) {
-        result.splice(i, 1)
+        result.unshift(result[i]);
       } else {
-        result.splice(i, 1,result[--i])
+        result.splice(i, 1,result[--i]);
       }
-    } 
-  })
+    } else {
+      i++;
+    }
+  }
   return result
 }
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
 module.exports = {
   transform
 };
